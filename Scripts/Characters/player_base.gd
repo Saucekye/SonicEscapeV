9e2cab9e23e681722f450aa496446ae83c1bc933
Zachary Therrien <ZTherrien.work@gmail.com>
2026-06-10 01:03:26 -0400

    New level chunks, obstacle adjustments and other changes

--- Scripts/Characters/player_base.gd
+++ Scripts/Characters/player_base.gd
@@ -980,10 +980,10 @@ func handle_floor_logic(delta):
 			hangable = false  # Can't initiate a hang while grounded
 		
 		# Shorter snap at low speed; longer snap at high speed to hug slopes better
-		if time_elapsed <= 50:
+		if motion.x <= 300:
 			floor_snap_length = 10
 		else:
-			floor_snap_length = 30
+			floor_snap_length = 35
 
 		falling = false
 		dashed = false
