bc8ff348c38ee42e6528a04976f3d62073d774b0
Zachary Therrien <ZTherrien.work@gmail.com>
2026-06-21 03:30:03 -0400

    New level chunks and changes. New functionality/change to springs.

--- Scripts/Obstacles/Springs/spring.gd
+++ Scripts/Obstacles/Springs/spring.gd
@@ -2,6 +2,8 @@
 extends Area2D
 
 @export var spring_velocity: Vector2 = Vector2(0, -1200)
+@export var ignore_rotation : bool = false ## Indicates whether to use the independent rotation value or not
+@export var independent_rotation : float = 0 ## The rotatinal value that the spring will send the player at, ignoring the rotations of the actual object. Usual when you want it angled one way, but want it to send a slightly different angle.
 @export var arc_points: int = 20
 @export var gravityarc: float = 1200.0
 @export var show_arc: bool = true
@@ -30,16 +32,16 @@ func _ready():
 func _notification(what):
 	if what == NOTIFICATION_TRANSFORM_CHANGED:
 		queue_redraw()
-		
-			
 	
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player"):
 		$AudioStreamPlayer2D.play()
 		$Sprite2D/AnimationPlayer.play("play")
-
+		var spring_rotation : float = self.rotation
+		if ignore_rotation:
+			spring_rotation = deg_to_rad(independent_rotation)
 		var player_body = area.get_parent()
-		player_body.apply_spring_boost(spring_velocity.rotated(rotation))
+		player_body.apply_spring_boost(spring_velocity.rotated(spring_rotation))
 
 
 
